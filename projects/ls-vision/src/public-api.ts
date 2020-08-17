d46a5d5c3f9b9bbe8f346b4e412f6eb39192c05c
Aaron Friedman <abf7d@users.noreply.github.com>
2020-08-17 10:52:25 -0400

    fix: class exports

--- projects/ls-vision/src/public-api.ts
+++ projects/ls-vision/src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/ls-vision.module';
 export * from './lib/directives/v-chart/v-chart.directive';
+export * from './lib/models/ls-vision';
+export * from './lib/models/vega-lite';
