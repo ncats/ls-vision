4e92715f2d30f590728e06c9544f18cfb05eae4b
Aaron Friedman <abf7d@users.noreply.github.com>
2020-09-21 16:38:11 -0400

    fix: linting

--- projects/ls-vision-app/src/assets/vega-charts/composite.ts
+++ projects/ls-vision-app/src/assets/vega-charts/composite.ts
@@ -1,4 +1,4 @@
-import { Coordinate, TimeUnit, BoxPlot, ArgmDef, Type, Align, XEnum, SelectionDefType, SingleDefUnitChannel } from 'ls-vision/lib/models/vega-lite';
+import { Coordinate, TimeUnit, BoxPlot, ArgmDef, Type, SelectionDefType, SingleDefUnitChannel } from 'ls-vision/lib/models/vega-lite';
 
 export const composite: Coordinate = {
     $schema: 'https://vega.github.io/schema/vega-lite/v4.json',
